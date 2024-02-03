bbe13d95a2110b70ab971e883e1365cadc96ba8a
maxchernigov <vladsnak23@gmail.com>
2024-02-03 15:29:32 +0200

    Initial commit

---
+++ pytest/pytest/seed.py
@@ -0,0 +1,31 @@
+import json
+from models import Authors, Quotes
+from connection import connect
+
+
+def load_json():
+    with open("authors.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+        for itr in data:
+            new_author = Authors()
+            new_author.fullname = itr["fullname"]
+            new_author.born_date = itr["born_date"]
+            new_author.description = itr["description"]
+            new_author.born_location = itr["born_location"]
+            new_author.save()
+
+    with open("quotes.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+        for itr in data:
+            authors = Authors(fullname=itr["author"])
+            new_quotes = Quotes()
+            new_quotes.author = authors
+            new_quotes.tags = itr["tags"]
+            new_quotes.quote = itr["quote"]
+            new_quotes.save()
+
+
+if __name__ == "__main__":
+    load_json()
